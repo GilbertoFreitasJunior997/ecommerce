325a7792119cf93e75324a4c30576dae23537b9f
GilbertoFreitas997 <gilbertofreitas997@gmail.com>
2023-06-15 19:50:30 -0300

    feat: added badge to product display category

--- src/components/product-display/index.tsx
+++ src/components/product-display/index.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
 import { ProductDisplayProps } from "./types";
+import { Link, useNavigate } from "react-router-dom";
 
 export const ProductDisplay: FC<ProductDisplayProps> = (props) => {
   const {
     label,
-    product: { title, thumbnail, description },
+    product: { title, thumbnail, description, category, id },
   } = props;
+
+  const navigate = useNavigate();
+
+  const productUrl = `product/${id}`;
+  const categoryUrl = `category/${category}`;
+
+  const goToProduct = () => {
+    navigate(productUrl);
+  };
 
   return (
     <div className="w-full mt-5">
@@ -15,15 +25,24 @@ export const ProductDisplay: FC<ProductDisplayProps> = (props) => {
           <img
             src={thumbnail}
             alt="Product image"
-            className="rounded-lg shadow"
+            className="transition-shadow rounded-lg shadow cursor-pointer hover:shadow-2xl"
+            onClick={goToProduct}
           />
         </div>
         <div className="flex flex-col justify-between py-2">
           <div>
-            <p className="text-lg">{title}</p>
+            <p className="flex items-center text-lg">
+              {title}
+              <Link
+                className="px-1 ml-2 text-sm transition-all rounded-full bg-emerald-200 text-emerald-700 hover:bg-emerald-300 hover:text-emerald-800 hover:scale-105"
+                to={categoryUrl}
+              >
+                {category}
+              </Link>
+            </p>
             <p className="">{description}</p>
           </div>
-          <div>SEE</div>
+          <Link to={productUrl}> SEE </Link>
         </div>
       </div>
     </div>
